49b09d2482b8ddb2b8eb0eda774b46e700987d12
Nagashankar <nagashankar0848@gmail.com>
2026-07-15 09:51:44 +0530

    commit message

--- Meta-Arguments/main.tf
+++ Meta-Arguments/main.tf
@@ -5,6 +5,8 @@ terraform {
       version = "~> 4.8.0" # Or current 4.x version
     }
   }
+
+
   backend "azurerm" {
   resource_group_name  = "kml_rg_main-8663b72e03404e1f"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `i
   storage_account_name = "day04217061"                      # Can be passed via `-backend-config=`"storage_account_name=<storage accoun
@@ -18,7 +20,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  # Terraform automatically picks up your 'az login' credentials here
+  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"  #subscription id of current kodekloud account
+
+  #az account show --query id -o tsv   to know the subscription-id
 }
 
 # 2. Declare the existing Resource Group using a Data Source
@@ -26,11 +30,11 @@ data "azurerm_resource_group" "existing_rg" {
   name = "kml_rg_main-8663b72e03404e1f"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "existing_rg" {
  
-  name                     = "nagashankar0848"
-  resource_group_name      = azurerm_resource_group.existing_rg.name
-  location                 = azurerm_resource_group.example.location # implicit dependency
+  name                     = "day04217061"
+  resource_group_name      = data.azurerm_resource_group.existing_rg.name
+  location                 = data.azurerm_resource_group.existing_rg.location # implicit dependency
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
