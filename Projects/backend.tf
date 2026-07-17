452a6cecd335cf6c3429c67caee079dc55ab1fe5
Nagashankar <nagashankar0848@gmail.com>
2026-07-17 22:49:41 +0530

    commit message

--- Projects/backend.tf
+++ Projects/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "kml_rg_main-22b40f613813458d"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name  = "kml_rg_main-08264393d1404474"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "nagastatefile"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
--- Projects/main.tf
+++ Projects/main.tf
@@ -1,6 +1,6 @@
 
 #  Declare the existing Resource Group using a Data Source
 data "azurerm_resource_group" "existing_rg" {
-  name = "kml_rg_main-22b40f613813458d"
+  name = "kml_rg_main-08264393d1404474"
 }
 
