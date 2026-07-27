ba1645a685f215728e03e21550b341bf649a6d35
Nagashankar <nagashankar0848@gmail.com>
2026-07-27 22:51:58 +0530

    commit message

--- Azure-Entra-ID/main.tf
+++ Azure-Entra-ID/main.tf
@@ -30,3 +30,20 @@ data "azuread_domains" "aad_domains" {}
 output "domain_names" {
   value = data.azuread_domains.aad_domains.domains.*.domain_name
 }
+
+
+# Now we are using locals here, so that we can use in multiple places in the code. We are using the local variable to store the domain names which we got from the azuread_domains data source. We are also using the local variable to store the domain names in a list format, so that we can use it in multiple places in the code.
+locals {
+  domain_name = data.azuread_domains.aad_domains.domains.*.domain_name
+  users = csvdecode(file("users.csv"))    # this is to decode the csv file and store it in a local variable. We can use this local variable in the resource block to create multiple users using the for_each meta-argument.
+}
+
+
+# Outputs
+  output "domain" {
+  value = local.domain_name
+  }
+ # this output is to print the usernames of the users which we are creating using the for_each meta-argument. We are using the local variable to get the first name and last name of the users and then we are concatenating them to get the full name of the user.
+output "username" {
+    value = [for user in local.users : "${user.first_name} ${user.last_name}"]
+  }
